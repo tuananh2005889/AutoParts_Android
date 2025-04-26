ffcb9db16624476e135ca9b1997d93cd169f6c86
tuananh2005889 <tuannguyen.120910@gmail.com>
2025-04-26 21:05:27 +0700

    update UI homepage

--- FrontEnd/app/src/main/java/com/example/frontend/MainActivity.kt
+++ FrontEnd/app/src/main/java/com/example/frontend/MainActivity.kt
@@ -7,12 +7,15 @@ import androidx.navigation.compose.rememberNavController
 import com.example.frontend.ui.navigation.AppNavHost
 import com.example.frontend.ui.theme.FrontEndTheme
 import dagger.hilt.android.AndroidEntryPoint
+import androidx.core.content.ContextCompat
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            window.statusBarColor = ContextCompat.getColor(this, R.color.dark_background)
+            window.navigationBarColor = ContextCompat.getColor(this, R.color.dark_background)
             FrontEndTheme {
                 val navController = rememberNavController()
                 AppNavHost(navController)
